25dc10107ffb1833851c741f76922bb352fd7253
Aulia Husnafia <111202315255@mhs.dinus.ac.id>
2026-06-25 11:57:37 +0700

    fix: simulate payment + add reviews endpoints

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', MeView.as_view(), name='me'),
-]
+]   
